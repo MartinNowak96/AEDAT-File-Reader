1ebcdb2a7deb521edcb707fda6adab2d3a44de0e
Martin Nowak <martinnowak96@outlook.com>
2018-10-02 22:09:25 -0400

    first

---
+++ AEDAT File Reader/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using AEDAT_File_Reader.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace AEDAT_File_Reader
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        ObservableCollection<AEDATData> tableData;
+        public MainPage()
+        {
+            tableData = AEDATDataManager.GetAEDATData();
+            this.InitializeComponent();
+        }
+
+
+        private async void selectFile_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            var picker = new Windows.Storage.Pickers.FileOpenPicker();
+            picker.ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail;
+            picker.SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.PicturesLibrary;
+            picker.FileTypeFilter.Add(".AEDAT");
+
+
+            Windows.Storage.StorageFile file = await picker.PickSingleFileAsync();
+        }
+    }
+}
---
+++ AEDAT File Reader/Models/AEDATData.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AEDAT_File_Reader.Models
+{
+    public class AEDATData
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public double startingTime { get; set; }
+        public double endingTime { get; set; }
+        public double eventCount { get; set; }
+        public double avgEventsPerSecond { get; set; }
+    }
+
+    public class AEDATDataManager
+    {
+        public static ObservableCollection<AEDATData> GetAEDATData()
+        {
+            var data = new ObservableCollection<AEDATData>();
+            // items.Add(new Item { name = "t-shirt", category = "Shirts", price="9.99", minQuantity ="10", TaxID1= "1"});
+            return data;
+        }
+    }
+}
