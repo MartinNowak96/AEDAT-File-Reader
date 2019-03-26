5d4b5416e779c0df22cb07885f3cf7fcd6a8b233
Anthony Beninati <abeninati01@manhattan.edu>
2019-03-26 16:17:40 -0400

    Video Settings
    Co-authored-by: Martin Nowak <martinnowak96@outlook.com>

--- AEDAT File Reader/videoPage.xaml.cs
+++ AEDAT File Reader/videoPage.xaml.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.Graphics.Canvas;
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Graphics.Imaging;
 using Windows.Media.Editing;
+using Windows.Media.MediaProperties;
 using Windows.Storage;
 using Windows.Storage.Pickers;
 using Windows.Storage.Streams;
@@ -20,18 +23,39 @@ namespace AEDAT_File_Reader
 	/// </summary>
 	/// 
 
-	public static class Colors {
+	public class Colors {
+
+		public Colors(string name, byte[] color)
+		{
+			this.Name = name;
+			this.Color = color;
+		}
+		public string Name;
+		public byte[] Color;
 		public static readonly byte[] Green = new byte[] { 0, 255, 0, 0 };
 		public static readonly byte[] Red = new byte[] { 255, 0, 0, 0 };
+		public static readonly byte[] Blue = new byte[] { 0, 0, 255, 0 };
+		public static readonly byte[] Gray = new byte[] { 127, 127, 127, 0 };
+		public static readonly byte[] White = new byte[] { 255, 255, 255, 0 };
 	}
+
 
 
 	public sealed partial class videoPage : Page
 	{
+		public ObservableCollection<Colors> colors;
 		public videoPage()
 		{
+			
+			colors = new ObservableCollection<Colors>();
+			colors.Add(new Colors("Green",Colors.Green));
+			colors.Add(new Colors("Red", Colors.Red));
+			colors.Add(new Colors("Blue", Colors.Blue));
+			colors.Add(new Colors("Gray", Colors.Gray));
+			colors.Add(new Colors("White", Colors.White));
 			InitializeComponent();
 		}
+		string previousValue = "100";
 
 		private async void SelectFile_Tapped(object sender, TappedRoutedEventArgs e)
 		{
@@ -39,19 +63,48 @@ namespace AEDAT_File_Reader
 			int maxFrames;             // Max number of frames in the reconstructed video
 
 			// Check for invalid input
+		
 			try
 			{
 				frameTime = Int32.Parse(frameTimeTB.Text);
-				maxFrames = Int32.Parse(maxFramesTB.Text);
-				if (maxFrames <= 0 || frameTime <= 0) {
-					throw new FormatException("Parameters must be greater than 0");
+				if (allFrameCheckBox.IsChecked == true)
+				{
+					maxFrames = 2147483647;
 				}
+				else
+				{
+					maxFrames = Int32.Parse(maxFramesTB.Text);
+				}
+				
 			}
 			catch (FormatException)
 			{
-				// TODO: Popup error message
+				ContentDialog noWifiDialog = new ContentDialog()
+				{
+					Title = "Invalid Input",
+					Content = "Please enter a valid number.",
+					CloseButtonText = "Close"
+				};
+
+				await noWifiDialog.ShowAsync();
 				return;
 			}
+
+			if (maxFrames <= 0 || frameTime <= 0)
+			{
+				ContentDialog noWifiDialog = new ContentDialog()
+				{
+					Title = "Invalid Input",
+					Content = "Enter a number greater than 0.",
+					CloseButtonText = "Close"
+				};
+
+				await noWifiDialog.ShowAsync();
+				return;
+			}
+
+			Colors onColor = onColorCombo.SelectedItem as Colors;
+			Colors offColor = offColorCombo.SelectedItem as Colors;
 
 			var picker = new FileOpenPicker
 			{
@@ -96,11 +149,11 @@ namespace AEDAT_File_Reader
 					UInt16[] XY = AedatUtilities.GetXYCords(currentDataEntry, cameraY);
 					if (AedatUtilities.GetEventType(currentDataEntry)) // ON event
 					{
-						AedatUtilities.setPixel(ref currentFrame, XY[0], XY[1], Colors.Green, cameraX);
+						AedatUtilities.setPixel(ref currentFrame, XY[0], XY[1], onColor.Color, cameraX);
 					}
 					else	// OFF event
 					{
-						AedatUtilities.setPixel(ref currentFrame, XY[0], XY[1], Colors.Red, cameraX);
+						AedatUtilities.setPixel(ref currentFrame, XY[0], XY[1], offColor.Color, cameraX);
 					}
 					if(lastTime == -999999)
 					{
@@ -134,11 +187,52 @@ namespace AEDAT_File_Reader
 				}
 
 				// Create video file
-				var sampleFile = await ApplicationData.Current.LocalFolder.CreateFileAsync("WBVideo.mp4");
+				var savePicker = new Windows.Storage.Pickers.FileSavePicker();
+				savePicker.SuggestedStartLocation =
+					Windows.Storage.Pickers.PickerLocationId.DocumentsLibrary;
+				// Dropdown of file types the user can save the file as
+				savePicker.FileTypeChoices.Add("MP4", new List<string>() { ".mp4" });
+				// Default file name if the user does not type one in or select a file to replace
+				savePicker.SuggestedFileName = file.DisplayName;
+				//var sampleFile = await ApplicationData.Current.LocalFolder.CreateFileAsync("WBVideo.mp4",Windows.Storage.CreationCollisionOption.ReplaceExisting);
+				Windows.Storage.StorageFile sampleFile = await savePicker.PickSaveFileAsync();
 				await composition.SaveAsync(sampleFile);
 				composition = await MediaComposition.LoadAsync(sampleFile);
-				await composition.RenderToFileAsync(sampleFile);
+
+				MediaEncodingProfile _MediaEncodingProfile = MediaEncodingProfile.CreateMp4(VideoEncodingQuality.HD720p);
+
+
+				////var encoding = new Windows.Media.MediaProperties.MediaEncodingProfile();
+				////encoding.Video.Bitrate = 10;
+				_MediaEncodingProfile.Video.Width = cameraX;
+				_MediaEncodingProfile.Video.Height = cameraY;
+				//_MediaEncodingProfile.Video.Bitrate = 300000000;
+
+				await composition.RenderToFileAsync(sampleFile, MediaTrimmingPreference.Precise, _MediaEncodingProfile);
+				//await composition.RenderToFileAsync(sampleFile);
+				mediaSimple.Source = new Uri("ms-appx:///WBVideo.mp4");
 			}
+		}
+
+		private void AllFrameCheckBox_Checked(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+		{
+			this.previousValue = maxFramesTB.Text;
+			maxFramesTB.IsReadOnly = true;
+			maxFramesTB.IsEnabled = false;
+			maxFramesTB.Text = "∞";
+		}
+
+		private void AllFrameCheckBox_Unchecked(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+		{
+			maxFramesTB.Text = this.previousValue;
+			maxFramesTB.IsReadOnly = false;
+			maxFramesTB.IsEnabled = true;
+		}
+
+		private void Page_Loaded(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+		{
+			onColorCombo.SelectedIndex = 0;
+			offColorCombo.SelectedIndex = 1;
 		}
 	}
 }
